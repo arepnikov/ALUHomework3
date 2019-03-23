918cdad6848f327169841e31e43386d767ba5ac5
Sir Alexsearch <aleksander.repnikov@gmail.com>
2019-03-23 13:55:05 +0100

    add toolbar

--- app/src/main/java/pl/daftacademy/androidlevelup/view/home/HomeActivity.kt
+++ app/src/main/java/pl/daftacademy/androidlevelup/view/home/HomeActivity.kt
@@ -1,7 +1,10 @@
 package pl.daftacademy.androidlevelup.view.home
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_home.*
 import pl.daftacademy.androidlevelup.R
 
 class HomeActivity : AppCompatActivity() {
@@ -9,5 +12,20 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem) =
+        when (item.itemId) {
+            android.R.id.home -> {
+                drawer.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
 }
